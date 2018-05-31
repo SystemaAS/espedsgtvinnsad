b8eec5c02454ae44a6650eea5f5fbbc28ada1248
Oscar de la Torre <oscar@systema.no>
2018-05-31 12:07:30 +0200

    Update

--- WebContent/WEB-INF/resources/js/tvinnsadgate.js
+++ WebContent/WEB-INF/resources/js/tvinnsadgate.js
@@ -1,22 +1,21 @@
   //this variable is a global jQuery var instead of using "$" all the time. Very handy
   var jq = jQuery.noConflict();
   var counterIndex = 0;
-  var BLOCKUI_OVERLAY_MESSAGE_DEFAULT = "Please wait...";
-  
+ 
   
   jq(function() {
 	  //General Header Menus
 	  jq('#alinkTopicListMenuExp').click(function() { 
-		  jq.blockUI({ message: BLOCKUI_OVERLAY_MESSAGE_DEFAULT});
+		  setBlockUI();
 	  });
 	  jq('#alinkTopicListMenuImp').click(function() { 
-		  jq.blockUI({ message: BLOCKUI_OVERLAY_MESSAGE_DEFAULT});
+		  setBlockUI();
 	  });
 	  jq('#alinkTopicListMenuNctsExp').click(function() { 
-		  jq.blockUI({ message: BLOCKUI_OVERLAY_MESSAGE_DEFAULT});
+		  setBlockUI();
 	  });
 	  jq('#alinkTopicListMenuNctsImp').click(function() { 
-		  jq.blockUI({ message: BLOCKUI_OVERLAY_MESSAGE_DEFAULT});
+		  setBlockUI();
 	  });
   });
   
