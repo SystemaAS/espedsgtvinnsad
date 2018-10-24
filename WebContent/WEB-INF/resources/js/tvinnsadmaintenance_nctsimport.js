6cf915a1c63ef848b269a4c5f5a6aa4811d07819
Fredrik Möller <fredrik@systema.no>
2018-10-24 11:30:09 +0200

    For NCTS Import: Trimmed mainList Localization support on dataTables and blockUI

--- WebContent/WEB-INF/resources/js/tvinnsadmaintenance_nctsimport.js
+++ WebContent/WEB-INF/resources/js/tvinnsadmaintenance_nctsimport.js
@@ -1,11 +1,6 @@
   //this variable is a global jQuery var instead of using "$" all the time. Very handy
   var jq = jQuery.noConflict();
   var counterIndex = 0;
-  var BLOCKUI_OVERLAY_MESSAGE_DEFAULT = "Please wait...";
-  
-  function setBlockUI(element){
-	  jq.blockUI({ css: { fontSize: '22px' }, message: BLOCKUI_OVERLAY_MESSAGE_DEFAULT});
-  }
   
   //-------------------
   //Datatables jquery
@@ -18,13 +13,12 @@
   }
   
   jq(document).ready(function() {
-      //init table (no ajax, no columns since the payload is already there by means of HTML produced on the back-end)
-      jq('#mainList').dataTable( {
+	  jq('#mainList').dataTable( {
     	  "dom": '<"top">t<"bottom"flip><"clear">',
-    	  "scrollY":    "350px",
-  		  "scrollCollapse":  false,
-  		  "columnDefs": [{ "type": "num", "targets": 0 }],
-  		  "lengthMenu": [ 75, 100]
+          "paging":   false,
+          "ordering": false,
+          "info":     false,
+          "searching":     false		  
   	  });
       
       //event on input field for search
