0b6e47e00e5d2d4570a1079482c4644a2d7460ee
Fredrik Möller <fredrik@systema.no>
2019-08-29 15:12:46 +0200

    Added sandbox controller for Express fortolling

---
+++ src/no/systema/tvinn/sad/manifest/controller/CodeSandboxController.java
@@ -0,0 +1,84 @@
+package no.systema.tvinn.sad.manifest.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import no.systema.jservices.common.dto.TransportationCompanyDto;
+import no.systema.main.util.AppConstants;
+
+/**
+ * Just to testing, remove when appropriate.
+ * 
+ * Full use of Spring integragted Jacksonmapper
+ * 
+ * @author Fredrik Möller
+ * @date Sep 01 , 2019
+ * 
+ */
+@Controller
+public class CodeSandboxController {
+	private static Logger logger = Logger.getLogger(CodeSandboxController.class.getName());
+	
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@GetMapping(value="sandboxTransportationCompany.do")
+	public String doSandboxTransportationCompany(@RequestParam(value = "id", required = false) String id) {
+		logger.info("sandboxTransportationCompany.do...");
+		
+		try {
+
+			List<TransportationCompanyDto> dtoList =  getTransportationCompany(id);
+			
+			logger.info("dtoList="+dtoList);
+
+		} catch (Exception e) {
+			logger.error("ERROR:", e);
+		}
+		
+		return "Kilroy was here...";
+		
+	}	
+
+//	@PostMapping(value="createManifest.do")
+//	public ModelAndView createManifest( @ModelAttribute ("record") KostaDto record, 
+//								@RequestParam(value = "action", required = true) Integer action,
+//								BindingResult bindingResult, HttpSession session, HttpServletRequest request){
+//
+//		return null;
+//		
+//	}
+	
+	/**
+	 * Usage: e.g. data for populating dropdowns.
+	 * 
+	 * @param id
+	 * @return
+	 */
+	
+	private List<TransportationCompanyDto> getTransportationCompany(String id) {
+		String BASE_URL = AppConstants.HTTP_ROOT_SERVLET_JSERVICES + "/syjservicestn-expft/transportationCompany.do";
+	    UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(BASE_URL)
+		        .queryParam("user", "SYSTEMA")
+		        .queryParam("id", id);
+		URI uri = builder.buildAndExpand().toUri();
+		logger.info("uri="+uri);
+		
+		
+		ResponseEntity<List<TransportationCompanyDto>> response = restTemplate.exchange(uri,HttpMethod.GET, null, new ParameterizedTypeReference<List<TransportationCompanyDto>>() {});
+		return response.getBody();		
+	}	
+	
+}
+
