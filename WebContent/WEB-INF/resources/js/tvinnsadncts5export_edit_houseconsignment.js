963180302f75dacbc6e6dc63d80b8e0288098639
sobekpeleteiro <64634843+sobekpeleteiro@users.noreply.github.com>
2023-06-23 11:16:55 +0200

    update

--- WebContent/WEB-INF/resources/js/tvinnsadncts5export_edit_houseconsignment.js
+++ WebContent/WEB-INF/resources/js/tvinnsadncts5export_edit_houseconsignment.js
@@ -81,123 +81,35 @@ function getItemData(record) {
 	  		var len = data.length;
 			for ( var i = 0; i < len; i++) {
 				jq('#lineNr').val(data[i].tcli)
+				
+				jq('#tcln').val(data[i].tcln)
+				jq('#tcdk').val(data[i].tcdk)
+				jq('#tcalk').val(data[i].tcalk)
+				jq('#tcblk').val(data[i].tcblk)
 				jq('#tcvktb').val(data[i].tcvktb)
 				jq('#tcucr').val(data[i].tcucr)
+				jq('#tcavd2').val(data[i].tcavd2)
+				jq('#tctdn2').val(data[i].tctdn2)
+				jq('#tcxext').val(data[i].tcxext)
+				jq('#tctaty').val(data[i].tctaty)
+				jq('#tctaid').val(data[i].tctaid)
+				jq('#tctalk').val(data[i].tctalk)
+				jq('#tcpdty').val(data[i].tcpdty)
 				
+				jq('#tcpdrf').val(data[i].tcpdrf)
+				jq('#tcpdin').val(data[i].tcpdin)
+				jq('#tcsdty').val(data[i].tcsdty)
+				jq('#tcsdrf').val(data[i].tcsdrf)
+				jq('#tcsdln').val(data[i].tcsdln)
+				jq('#tcsdin').val(data[i].tcsdin)
+				jq('#tctdty').val(data[i].tctdty)
+				jq('#tctdrf').val(data[i].tctdrf)
+				jq('#tcadty').val(data[i].tcadty)
+				jq('#tcadrf').val(data[i].tcadrf)
+				jq('#tcaicd').val(data[i].tcaicd)
+				jq('#tcaitx').val(data[i].tcaitx)
+				jq('#tctrch').val(data[i].tctrch)
 				
-				/*
-				jq('#lineNr').val(data[i].tvli)
-				jq('#tvli').val(data[i].tvli); 
-				jq('#tvvnt').val(data[i].tvvnt);jq('#tvvt').val(data[i].tvvt);
-				jq('#tvvt2').val(data[i].tvvt2);jq('#tvvt3').val(data[i].tvvt3);
-				jq('#tvvt4').val(data[i].tvvt4);jq('#tvvt5').val(data[i].tvvt5);
-				
-				jq('#tvtdr').val(data[i].tvtdr);jq('#tvdk').val(data[i].tvdk); 
-				jq('#tvvktb').val(data[i].tvvktb); jq('#tvvktn').val(data[i].tvvktn); 
-				jq('#tvnt').val(data[i].tvnt);jq('#tvdref').val(data[i].tvdref);
-				jq('#tvdsk').val(data[i].tvdsk);
-				
-				jq('#tvdty').val(data[i].tvdty); 
-				jq('#tvalk').val(data[i].tvalk);
-				jq('#tvblk').val(data[i].tvblk);
-				jq('#tveh').val(data[i].tveh);
-				
-				//Extra section
-				jq('#tvvtsk').val(data[i].tvvtsk);jq('#tvtdt').val(data[i].tvtdt);
-				jq('#tvtdsk').val(data[i].tvtdsk);jq('#tvtdo').val(data[i].tvtdo);
-				jq('#tvtdos').val(data[i].tvtdos);
-				jq('#tvdosk').val(data[i].tvdosk);jq('#tvmtxt').val(data[i].tvmtxt);
-				jq('#tvmsk').val(data[i].tvmsk);jq('#tvtlo').val(data[i].tvtlo);
-				jq('#tvmsk').val(data[i].tvmsk);jq('#tvtlo').val(data[i].tvtlo);
-				jq('#tvexkd').val(data[i].tvexkd);jq('#tvexlk').val(data[i].tvexlk);
-				jq('#tvavd2').val(data[i].tvavd2);jq('#tvtdn2').val(data[i].tvtdn2);
-				jq('#tvcnr').val(data[i].tvcnr);jq('#tvmn').val(data[i].tvmn);
-				jq('#tvmnsk').val(data[i].tvmnsk);jq('#tvtdn2').val(data[i].tvtdn2);
-				jq('#tvnteh').val(data[i].tvnteh);
-				jq('#tvcnr2').val(data[i].tvcnr2);jq('#tvcnr3').val(data[i].tvcnr3);
-				jq('#tvcnr4').val(data[i].tvcnr4);jq('#tvcnr5').val(data[i].tvcnr5);
-				jq('#tvcnr6').val(data[i].tvcnr6);
-				jq('#tvfv').val(data[i].tvfv);jq('#tvfvnt').val(data[i].tvfvnt);
-				jq('#tvkns').val(data[i].tvkns);
-				jq('#tvnas').val(data[i].tvnas);
-				jq('#tvads1').val(data[i].tvads1);jq('#tvpns').val(data[i].tvpns);
-				jq('#tvpss').val(data[i].tvpss);jq('#tvlks').val(data[i].tvlks);
-				jq('#tvsks').val(data[i].tvsks);jq('#tvtins').val(data[i].tvtins);
-				jq('#tvknk').val(data[i].tvknk);
-				jq('#tvnak').val(data[i].tvnak);
-				jq('#tvadk1').val(data[i].tvadk1);jq('#tvpnk').val(data[i].tvpnk);
-				jq('#tvpsk').val(data[i].tvpsk);jq('#tvlkk').val(data[i].tvlkk);
-				jq('#tvskk').val(data[i].tvskk);jq('#tvtink').val(data[i].tvtink);
-				jq('#tvln').val(data[i].tvln);jq('#tvrefl').val(data[i].tvrefl);
-				
-				//rubrik 31 (extra Godsmärke)
-				jq('#tvmn2').val(data[i].tvmn2);jq('#tveh2').val(data[i].tveh2);
-				jq('#tvnt2').val(data[i].tvnt2);jq('#tvnteh2').val(data[i].tvnteh2);
-				jq('#tvmn3').val(data[i].tvmn3);jq('#tveh3').val(data[i].tveh3);
-				jq('#tvnt3').val(data[i].tvnt3);jq('#tvnteh3').val(data[i].tvnteh3);
-				jq('#tvmn4').val(data[i].tvmn4);jq('#tveh4').val(data[i].tveh4);
-				jq('#tvnt4').val(data[i].tvnt4);jq('#tvnteh4').val(data[i].tvnteh4);
-				jq('#tvmn5').val(data[i].tvmn5);jq('#tveh5').val(data[i].tveh5);
-				jq('#tvnt5').val(data[i].tvnt5);jq('#tvnteh5').val(data[i].tvnteh5);
-				//end rubrik 31
-				
-				//rubrik 44 (Supplerende Opplysninger) - Multilines
-				jq('#tvdty2').val(data[i].tvdty2);jq('#tvdref2').val(data[i].tvdref2);
-				jq('#tvdsk2').val(data[i].tvdsk2);jq('#tvdo2').val(data[i].tvdo2);
-				jq('#tvdosk2').val(data[i].tvdosk2);
-				jq('#tvmtxt2').val(data[i].tvmtxt2);jq('#tvmsk2').val(data[i].tvmsk2);
-				jq('#tvtlo2').val(data[i].tvtlo2);jq('#tvexkd2').val(data[i].tvexkd2);
-				jq('#tvexlk2').val(data[i].tvexlk2);
-				
-				jq('#tvdty3').val(data[i].tvdty3);jq('#tvdref3').val(data[i].tvdref3);
-				jq('#tvdsk3').val(data[i].tvdsk3);jq('#tvdo3').val(data[i].tvdo3);
-				jq('#tvdosk3').val(data[i].tvdosk3);
-				jq('#tvmtxt3').val(data[i].tvmtxt3);jq('#tvmsk3').val(data[i].tvmsk3);
-				jq('#tvtlo3').val(data[i].tvtlo3);jq('#tvexkd3').val(data[i].tvexkd3);
-				jq('#tvexlk3').val(data[i].tvexlk3);
-				
-				jq('#tvdty4').val(data[i].tvdty4);jq('#tvdref4').val(data[i].tvdref4);
-				jq('#tvdsk4').val(data[i].tvdsk4);jq('#tvdo4').val(data[i].tvdo4);
-				jq('#tvdosk4').val(data[i].tvdosk4);
-				jq('#tvmtxt4').val(data[i].tvmtxt4);jq('#tvmsk4').val(data[i].tvmsk4);
-				jq('#tvtlo4').val(data[i].tvtlo4);jq('#tvexkd4').val(data[i].tvexkd4);
-				jq('#tvexlk4').val(data[i].tvexlk4);
-				
-				
-				jq('#tvdty_readonly').val(data[i].tvdty);
-				jq('#tvdref_readonly').val(data[i].tvdref);
-				jq('#tvdsk_readonly').val(data[i].tvdsk);
-				jq('#tvdo').val(data[i].tvdo);
-				// End rubrik 44
-				
-				
-				//Sikkerhet - Security section
-				jq('#tvtkbm').val(data[i].tvtkbm);
-				jq('#tvkref').val(data[i].tvkref);
-				jq('#tvfgkd').val(data[i].tvfgkd);
-				jq('#tvknss').val(data[i].tvknss);
-				jq('#tvnass').val(data[i].tvnass);
-				jq('#tvadss1').val(data[i].tvadss1);
-				jq('#tvknss').val(data[i].tvknss);
-				jq('#tvpnss').val(data[i].tvpnss);
-				jq('#tvpsss').val(data[i].tvpsss);
-				jq('#tvlkss').val(data[i].tvlkss);
-				jq('#tvskss').val(data[i].tvskss);
-				jq('#tvtinss').val(data[i].tvtinss);
-				jq('#tvknks').val(data[i].tvknks);
-				jq('#tvnaks').val(data[i].tvnaks);
-				jq('#tvadks1').val(data[i].tvadks1);
-				jq('#tvknks').val(data[i].tvknks);
-				jq('#tvpnks').val(data[i].tvpnks);
-				jq('#tvpsks').val(data[i].tvpsks);
-				jq('#tvlkks').val(data[i].tvlkks);
-				jq('#tvskks').val(data[i].tvskks);
-				jq('#tvtinks').val(data[i].tvtinks);
-				
-				
-				//debug information on Fetch item
-				jq('#debugPrintlnAjaxItemFetchInfo').text(data[i].debugPrintlnAjax);
-				*/
 			}
 	  	  },
 	  	  error: function() {
